3b269eb9f1f016a91ae149812080f987b52eac5a
Rhenel Jhon Sajol <sajol.rhenel123@gmail.com>
2026-01-17 11:41:16 +0000

    Division Function

--- calculator.py
+++ calculator.py
@@ -7,15 +7,27 @@ def subtract(a, b):
     return a - b
 
 def multiply(a, b):
-    return a*b
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
 
 while True:
     print("\nChoose an operation.")
     print("1 - Add")
     print("2 - Subtract")
     print("3 - Multiply")
+    print("4 - Divide")
+    print("5 - Exit")
 
     choice = input("Enter choice: ")
+      
+    if choice == "5":
+        print("Program ended.")
+        break
+
 
     a = float(input("Enter first number: "))
     b = float(input("Enter second number: "))
@@ -26,6 +38,8 @@ while True:
         print("Result:", subtract(a, b))
     elif choice == "3":
         print("Result: ", multiply(a, b))
+    elif choice == "4":
+        print("Result:", divide(a, b))
     else:
         print("Invalid choice")
-
+   
